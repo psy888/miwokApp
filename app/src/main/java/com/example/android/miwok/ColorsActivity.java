2c845ea0c3549bb5f662429bc58a860123d45170
psy888 <lpsyl888l@yandex.ru>
2018-08-06 17:25:44 +0300

    add vocab for each category, create one reusable xml activity.

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,13 +1,36 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.word_list);
+        //Words array
+        ArrayList<Word> words = new ArrayList<>();
+        words.add(new Word("weṭeṭṭi","red"));
+        words.add(new Word("chokokki","green"));
+        words.add(new Word("ṭakaakki","brown"));
+        words.add(new Word("ṭopoppi","grey"));
+        words.add(new Word("kululli","black"));
+        words.add(new Word("kelelli","white"));
+        words.add(new Word("ṭopiisә","dusty yellow"));
+        words.add(new Word("chiwiiṭә","mustard yellow"));
+
+
+        // Создаем {@link AndroidFlavorAdapter}, источником данных которого является список
+        // {@link AndroidFlavor} s. Адаптер знает, как создавать представления элементов списка для каждого элемента
+        // в списке.
+        WordAdapter itemsAdapter = new WordAdapter(this, words);
+
+        // Получить ссылку на ListView и прикрепить адаптер к спискуView.
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
 }
--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -1,13 +1,38 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.word_list);
+        //Words array
+        ArrayList<Word> words = new ArrayList<>();
+        words.add(new Word("әpә","father"));
+        words.add(new Word("әṭa","mother"));
+        words.add(new Word("angsi","son"));
+        words.add(new Word("tune","daughter"));
+        words.add(new Word("taachi","older brother"));
+        words.add(new Word("chalitti","younger brother"));
+        words.add(new Word("teṭe","older sister"));
+        words.add(new Word("kolliti","younger sister"));
+        words.add(new Word("ama","grandmother"));
+        words.add(new Word("paapa","grandfather"));
+
+
+        // Создаем {@link AndroidFlavorAdapter}, источником данных которого является список
+        // {@link AndroidFlavor} s. Адаптер знает, как создавать представления элементов списка для каждого элемента
+        // в списке.
+        WordAdapter itemsAdapter = new WordAdapter(this, words);
+
+        // Получить ссылку на ListView и прикрепить адаптер к спискуView.
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
 }
